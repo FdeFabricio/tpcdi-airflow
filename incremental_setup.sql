b10ea237093d82fc163ac23334fc65e8c4ee3e84
Ali <eng.aliarous@gmail.com>
2019-12-20 16:04:59 +0100

    INCREMENTAL UPDATE: FactMarketHistory

--- incremental_setup.sql
+++ incremental_setup.sql
@@ -63,3 +63,110 @@ BEGIN
 END;
 $$
 delimiter ;
+
+
+-- -----------------------------------------------------------------------
+
+ALTER TABLE tpcdi.FactMarketHistory ADD COLUMN (
+    Date DATE,
+    Symbol CHAR(15),
+    FiftyTwoWeekLowDate DATE,
+    FiftyTwoWeekHighDate DATE,
+    prev1_quarter NUMERIC(1),
+    prev2_quarter NUMERIC(1),
+    prev3_quarter NUMERIC(1),
+    prev4_quarter NUMERIC(1),
+    prev1_year NUMERIC(4),
+    prev2_year NUMERIC(4),
+    prev3_year NUMERIC(4),
+    prev4_year NUMERIC(4)
+);
+
+
+DROP TRIGGER IF EXISTS tpcdi.INC_FactMarketHistory;
+delimiter $$
+CREATE TRIGGER `INC_FactMarketHistory` BEFORE INSERT ON `FactMarketHistory`
+FOR EACH ROW
+BEGIN
+    DECLARE _sec_id, _cmp_id NUMERIC(11);
+    DECLARE _dividend NUMERIC(10,2);
+	DECLARE _FiftyTwoMoreHigh NUMERIC(8,2);
+	DECLARE _FiftyTwoMoreHigh_SK_Date NUMERIC(11);
+	DECLARE _FiftyTwoLessLow NUMERIC(8,2);
+	DECLARE _FiftyTwoLessLow_SK_Date NUMERIC(11);
+
+    SELECT DimSecurity.SK_SecurityID, DimSecurity.SK_CompanyID, DimSecurity.Dividend
+    INTO @_sec_id, @_cmp_id, @_dividend
+    FROM DimSecurity
+    WHERE DimSecurity.Symbol = NEW.Symbol AND
+          DimSecurity.IsCurrent = 1;
+
+    SET NEW.SK_SecurityID = @_sec_id;
+    SET NEW.SK_CompanyID = @_cmp_id;
+
+	
+	SELECT FiftyTwoWeekHigh, SK_FiftyTwoWeekHighDate
+	INTO @_FiftyTwoMoreHigh, @_FiftyTwoMoreHigh_SK_Date
+	FROM FactMarketHistory
+	WHERE FactMarketHistory.SK_SecurityID = @_sec_id AND
+		  DATEDIFF(
+			(SELECT DateValue FROM DimDate WHERE SK_DateID = FactMarketHistory.SK_DateID),
+			New.Date
+		  ) <=365 
+		  AND FactMarketHistory.FiftyTwoWeekHigh > NEW.FiftyTwoWeekHigh;
+	
+	
+    SET NEW.SK_DateID = (
+        SELECT DimDate.SK_DateID
+        FROM DimDate
+        WHERE DimDate.DateValue = NEW.Date
+    );
+
+	
+	SET NEW.FiftyTwoWeekHigh = IFNULL(@_FiftyTwoMoreHigh,NEW.FiftyTwoWeekHigh);
+
+    SET NEW.SK_FiftyTwoWeekHighDate = IFNULL(@_FiftyTwoMoreHigh_SK_Date,(
+        SELECT DimDate.SK_DateID
+        FROM DimDate
+        WHERE DimDate.DateValue = NEW.FiftyTwoWeekHighDate
+    ));
+
+
+	SELECT FiftyTwoWeekLow, SK_FiftyTwoWeekHighLowDate
+	INTO @_FiftyTwoLessLow, @_FiftyTwoLessLow_SK_Date
+	FROM FactMarketHistory
+	WHERE FactMarketHistory.SK_SecurityID = @_sec_id AND
+		  DATEDIFF(
+			(SELECT DateValue FROM DimDate WHERE SK_DateID = FactMarketHistory.SK_DateID),
+			New.Date
+		  ) <=365 
+		  AND FactMarketHistory.FiftyTwoWeekLow < NEW.FiftyTwoWeekLow;
+	
+	SET NEW.FiftyTwoWeekLow = IFNULL(@_FiftyTwoLessLow,NEW.FiftyTwoWeekLow);
+
+
+    SET NEW.SK_FiftyTwoWeekLowDate = IFNULL(@_FiftyTwoLessLow_SK_Date,(
+        SELECT DimDate.SK_DateID
+        FROM DimDate
+        WHERE DimDate.DateValue = NEW.FiftyTwoWeekLowDate
+    ));
+
+    SET NEW.PERatio = (
+        SELECT NEW.ClosePrice / SUM(Financial.FI_BASIC_EPS)
+        FROM Financial
+        WHERE Financial.SK_CompanyID = @_cmp_id AND (
+              (Financial.FI_YEAR = NEW.prev1_year AND Financial.FI_QTR = NEW.prev1_quarter ) OR
+              (Financial.FI_YEAR = NEW.prev2_year AND Financial.FI_QTR = NEW.prev2_quarter ) OR
+              (Financial.FI_YEAR = NEW.prev3_year AND Financial.FI_QTR = NEW.prev3_quarter ) OR
+              (Financial.FI_YEAR = NEW.prev4_year AND Financial.FI_QTR = NEW.prev4_quarter ))
+    );
+
+    IF (NEW.PERatio IS NULL) THEN
+        INSERT INTO DImessages (BatchID, MessageSource, MessageText, MessageType, MessageData)
+        VALUES (1, "FactMarketHistory", "No earnings for company", "Alert", CONCAT("DM_S_SYMB = ", NEW.Symbol));
+    END IF;
+
+    SET NEW.Yield = @_dividend * 100 / NEW.ClosePrice;
+END;
+$$
+delimiter ;
